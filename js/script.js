5bc49d4f2ee86141d516604ed1d9385c894fe2e6
Cheryl Davis <cheryldavis@cheryls-air.attlocal.net>
2019-04-20 15:14:13 -0700

    ready for review

--- js/script.js
+++ js/script.js
@@ -1,35 +1,34 @@
-$(document).ready(function(){
-//var host = 'https://shreecheryl.github.io/SpencerTest';
-var host = 'http://127.0.0.1:63856';
-var pages = [host + '/powerCouple.html', host + '/codeBlack2.html', host + '/sonsOfAnarchy.html'];
-console.log(host);
+$(document).ready(function () {
+    "use strict";
+    var host = 'https://shreecheryl.github.io/SpencerTest',
+// var host = 'http://127.0.0.1:63856',
+        pages = [host + '/powerCouple.html', host + '/codeBlack.html', host + '/sonsOfAnarchy.html'],
+        behind = document.getElementById('prev'),
+        forward = document.getElementById('next');
 
-var behind = document.getElementById('prev');
-var forward = document.getElementById('next');
+    function prev(current, pages) {
+        var index = pages.indexOf(current);
+        if (index === 0) {
+            return pages[pages.length - 1];
+        }
+        return pages[index - 1];
+    }
 
-function prev(current, pages) {
-  var index = pages.indexOf(current);
-  if (index === 0) {
-    return pages[pages.length - 1];
-  }
-  return pages[index - 1];
-}
+    function next(current, pages) {
+        var index = pages.indexOf(current);
+        if (index === pages.length - 1) {
+            return pages[0];
+        }
+        return pages[index + 1];
+    }
 
-function next(current, pages) {
-  var index = pages.indexOf(current);
-  if (index === pages.length - 1) {
-    return pages[0];
-  }
-  return pages[index + 1];
-}
+    behind.addEventListener('click', function () {
+        var newUrl = prev(host + window.location.pathname, pages);
+        window.location.href = newUrl;
+    });
 
-behind.addEventListener('click', function() {
-  var newUrl = prev(host + window.location.pathname, pages);
-  window.location.href = newUrl;
-})
-
-forward.addEventListener('click', function() {
-  var newUrl = next(host + window.location.pathname, pages);
-  window.location.href = newUrl;
-}) 
+    forward.addEventListener('click', function () {
+        var newUrl = next(host + window.location.pathname, pages);
+        window.location.href = newUrl;
+    }); 
 }); // end document ready
